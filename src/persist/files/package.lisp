488bebfb36ea2859e63a9ec94018304594199fbd
Patrick Stein <pat@nklein.com>
2019-09-27 19:54:07 -0500

    Moved RAFT-PERSIST-FILES to its own repository.

--- src/persist/files/package.lisp
+++
@@ -1,6 +0,0 @@
-;;; src/persist/files/package.lisp
-
-(defpackage #:raft-persist-files
-  (:use #:raft-persist #:cl)
-  ;; from creation
-  (:export #:make-persist-files))
--- test/persist/files/package.lisp
+++
@@ -1,4 +0,0 @@
-;;; test/persist/files/package.lisp
-
-(defpackage #:raft-persist-files-test
-  (:use #:raft-persist-files #:raft-persist #:cl))
